defdfd6642ef717b5e874f461e6f30abca202527
frieda_usha <hallofrieda@gmail.com>
2017-11-07 17:56:08 +0700

    .gitignore

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ from knn import KNN
 from linereg import line_reg
 
 iris = datasets.load_iris()
-iris_X = iris.data
-iris_Y = iris.target
-np.unique(iris_Y)
-
-np.random.seed(0)
-indices = np.random.permutation(len(iris_X))
-iris_X_train = iris_X[indices[:-10]]
-iris_y_train = iris_Y[indices[:-10]]
-iris_X_test  = iris_X[indices[-10:]]
-iris_y_test  = iris_Y[indices[-10:]]
 
 print ('Choose Method :')
 print ('1. K-Nearest Neighbour')
