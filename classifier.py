1b68fe3aace3430f5cdbd7dadb1e8974a3a789a0
frieda_usha <hallofrieda@gmail.com>
2017-11-22 18:12:41 +0700

    add neuron

--- classifier.py
+++ classifier.py
@@ -1,57 +1,50 @@
 from sklearn import tree
 from sklearn.metrics import accuracy_score
-from sklearn import linear_model
 from sklearn import neighbors
 from operator import truediv
-from sklearn.metrics import recall_score, average_precision_score
+from sklearn.neural_network import MLPClassifier
+import numpy as np
 
 def measure(y_actual, y_predicted):
     TP = 0
     FP = 0
     TN = 0
     FN = 0
-    # print y_actual
-    # print y_predicted
-    #print str(len(y_actual))
-    print str(len(y_predicted))
-    for j in range(3):
+    clazz = len(np.unique(y_predicted))
+    for j in range(clazz):
         for i in range(len(y_predicted)):
-            if y_actual[i]==y_predicted[i]==j:
-               TP += 1
+            if y_actual[i] == y_predicted[i] == j:
+                TP += 1
         for i in range(len(y_predicted)):
-            if y_predicted[i]==j and y_actual[i]==y_predicted[i]:
-               FP += 1
+            if y_predicted[i] == j and y_actual[i] == y_predicted[i]:
+                FP += 1
         for i in range(len(y_predicted)):
-            if y_actual[i]==y_predicted[i]!=j:
-               TN += 1
+            if y_actual[i] == y_predicted[i] != j:
+                TN += 1
         for i in range(len(y_predicted)):
-            if y_predicted[i]!=j and y_actual[i]==y_predicted[i]:
-               FN += 1
-    TP = truediv(TP, len(y_predicted))/3*100
-    FN = truediv(FN, len(y_predicted))/3*100
-    print 'The sensitivity percentage is : ' + str(TP)
-    print 'The specificity percentage is : ' + str(FN)
+            if y_predicted[i] != j and y_actual[i] == y_predicted[i]:
+                FN += 1
+    TP = truediv(TP, len(y_predicted)) / clazz * 100
+    FN = truediv(FN, len(y_predicted)) / clazz * 100
+    return TP, FN
+    # print 'The sensitivity percentage is : ' + str(TP)
+    # print 'The specificity percentage is : ' + str(FN)
 
 def dectree(iris_x_train, iris_x_test, iris_y_train, iris_y_test):
-
     dec = tree.DecisionTreeClassifier()
     dec.fit(iris_x_train, iris_y_train)
-    predicted =  dec.predict(iris_x_test)
-    measure(iris_y_test, predicted)
-    #print str(sum(recall_score(iris_y_test, predicted, average=None))/3)
-    return accuracy_score(iris_y_test, predicted)*100
-
-def line_reg(iris_x_train, iris_x_test, iris_y_train, iris_y_test):
-
-    regr = linear_model.LinearRegression()
-    regr.fit(iris_x_train, iris_y_train)
-    predicted = regr.predict(iris_x_test)
-    return regr.score(iris_x_test, iris_y_test)*100
+    predicted = dec.predict(iris_x_test)
+    return accuracy_score(iris_y_test, predicted) * 100, measure(iris_y_test, predicted)
 
 def KNN(iris_x_train, iris_x_test, iris_y_train, iris_y_test):
-
-    knn = neighbors.KNeighborsClassifier()
+    knn = neighbors.KNeighborsClassifier(n_neighbors=5)
     knn.fit(iris_x_train, iris_y_train)
     predicted = knn.predict(iris_x_test)
-    measure(iris_y_test, predicted)
-    return knn.score(iris_x_test, iris_y_test)*100
+    return knn.score(iris_x_test, iris_y_test) * 100 , measure(iris_y_test, predicted)
+
+
+def NeuralNetwork(iris_x_train, iris_x_test, iris_y_train, iris_y_test):
+    nn = MLPClassifier()
+    nn.fit(iris_x_train, iris_y_train)
+    predicted = nn.predict(iris_x_test)
+    return nn.score(iris_x_test, iris_y_test) * 100 , measure(iris_y_test, predicted)
--- main.py
+++ main.py
@@ -1,28 +1,65 @@
 import numpy as np
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn import datasets
-import random
-from classifier import line_reg, dectree, KNN
+from classifier import dectree, KNN, NeuralNetwork
 
-iris = datasets.load_iris()
-
-np.random.seed(0)
+iris = datasets.load_breast_cancer()
 iris_X = iris.data
 iris_Y = iris.target
-indices = np.random.permutation(len(iris_X))
-rnd = random.randint(20, 100)
+sum_KNN = 0.0
+sum_dectree = 0.0
+sum_NN = 0.0
+sum_recall_knn = 0.0
+sum_spec_knn = 0.0
+sum_recall_dec = 0.0
+sum_spec_dec = 0.0
+sum_recall_nn = 0.0
+sum_spec_nn = 0.0
 
-iris_x_train = iris_X[indices[:-rnd]]
-iris_y_train = iris_Y[indices[:-rnd]]
-iris_x_test = iris_X[indices[-rnd:]]
-iris_y_test = iris_Y[indices[-rnd:]]
-np.unique(iris_Y)
+sizecv = 5
+kf = StratifiedKFold(n_splits=sizecv, shuffle=True, random_state=123)
+for train, test in kf.split(iris_X, iris_Y):
+    iris_x_train = iris_X[train]
+    iris_y_train = iris_Y[train]
+    iris_x_test = iris_X[test]
+    iris_y_test = iris_Y[test]
+    #print ('KNN')
+    sumtemp = []
+    val_KNN , sumtemp = KNN(iris_x_train, iris_x_test, iris_y_train, iris_y_test)
+    sum_KNN += val_KNN
+    sum_recall_knn += sumtemp[0]
+    sum_spec_knn += sumtemp[1]
+    # print ('Accuracy score of knn :') + str(val_KNN)
+    # print ('')
 
-print ('KNN')
-print ('Accuracy score of knn :') + str(KNN(iris_x_train, iris_x_test, iris_y_train, iris_y_test))
+    #print ('Decision Tree')
+    sumtemp = []
+    val_dectree, sumtemp = dectree(iris_x_train, iris_x_test, iris_y_train, iris_y_test)
+    sum_dectree += val_dectree
+    sum_recall_dec += sumtemp[0]
+    sum_spec_dec += sumtemp[1]
+    # print ('Accuracy score of decision tree :') + str(val_dectree)
+    # print ('')
+
+    # print ('Neural Network')
+    sumtemp = []
+    val_NN, sumtemp = NeuralNetwork(iris_x_train, iris_x_test, iris_y_train, iris_y_test)
+    sum_NN += val_NN
+    sum_recall_nn += sumtemp[0]
+    sum_spec_nn += sumtemp[1]
+    # print ('Accuracy score of decision tree :') + str(val_NN)
+    # print ('')
+print ('KNN :')
+print ('Average sensitivity : ') + str(sum_recall_knn/sizecv)
+print ('Average specificity : ') + str(sum_spec_knn/sizecv)
+print ('Average score of    : ') + str(sum_KNN / sizecv)
+print (' ')
+print ('Decision Tree :')
+print ('Average sensitivity : ') + str(sum_recall_dec/sizecv)
+print ('Average specificity : ') + str(sum_spec_dec/sizecv)
+print ('Average score of decision tree : ') + str(sum_dectree / sizecv)
 print ('')
-print ('Decision Tree')
-print ('Accuracy score of decision tree :') + str(dectree(iris_x_train, iris_x_test, iris_y_train, iris_y_test))
-print ('')
-print ('Linear Regression')
-print ('Accuracy score of linear regression :') + str(line_reg(iris_x_train, iris_x_test, iris_y_train, iris_y_test))
-print ('')
+print ('Neural Network :')
+print ('Average sensitivity : ') + str(sum_recall_nn/sizecv)
+print ('Average specificity : ') + str(sum_spec_nn/sizecv)
+print ('Average score of Neural Network: ') + str(sum_NN / sizecv)
